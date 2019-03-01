87c01213809414eb9b089b9fda95b19f5d90ebb7
Alex Gittings <axiansdeveloper@axians.co.uk>
2019-03-01 23:12:45 +0000

    initial

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="silverpeak",
+    version="0.0.1",
+    author="Alex Gittings, Adam Jarvis",
+    author_email="alexgit@hotmail.co.uk, adamjohnjarvis1@gmail.com",
+    description="A package to allow you to interact with the Silver Peak Orchestrator API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/minitriga/silverpeak_python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ silverpeak/__init__.py
@@ -0,0 +1,2 @@
+name = "silverpeak"
+from .silverpeak import *
